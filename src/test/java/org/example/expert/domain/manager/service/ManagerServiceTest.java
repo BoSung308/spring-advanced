013d6f4544849836bcbfe38a48a1ad917adabf03
BoSung308 <qhtjd2719@naver.com>
2024-09-12 00:11:42 +0900

    Level 2-6, 테스트 연습 2

--- src/test/java/org/example/expert/domain/manager/service/ManagerServiceTest.java
+++ src/test/java/org/example/expert/domain/manager/service/ManagerServiceTest.java
@@ -46,8 +46,9 @@ class ManagerServiceTest {
 
         // when & then
         InvalidRequestException exception = assertThrows(InvalidRequestException.class, () -> managerService.getManagers(todoId));
-        assertEquals("Manager not found", exception.getMessage());
+        assertEquals("TOdo not found", exception.getMessage());
     }
+
 
     @Test
     void todo의_user가_null인_경우_예외가_발생한다() {
@@ -65,7 +66,7 @@ class ManagerServiceTest {
 
         // when & then
         InvalidRequestException exception = assertThrows(InvalidRequestException.class, () ->
-            managerService.saveManager(authUser, todoId, managerSaveRequest)
+                managerService.saveManager(authUser, todoId, managerSaveRequest)
         );
 
         assertEquals("담당자를 등록하려고 하는 유저가 일정을 만든 유저가 유효하지 않습니다.", exception.getMessage());
